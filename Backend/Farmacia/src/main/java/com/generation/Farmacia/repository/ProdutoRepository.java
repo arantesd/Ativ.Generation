e773a9733c2357810f762b0d9bb8adccc05f5bf6
unknown <danielam.arantes@gmail.com>
2021-03-24 10:34:13 -0300

    CRUD Farmacia

--- Backend/Farmacia/src/main/java/com/generation/Farmacia/repository/ProdutoRepository.java
+++ Backend/Farmacia/src/main/java/com/generation/Farmacia/repository/ProdutoRepository.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.generation.Farmacia.model.Produto;
 
-public interface ProdutoRepository extends JpaRepository<Produto, String> {
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 	public List<Produto>findAllByDescricaoTituloContainingIgnoreCase(String descricaoTitulo);
 
 }
